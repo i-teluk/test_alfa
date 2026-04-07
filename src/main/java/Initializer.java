b38cc4387e6d892ef10b8abe3f6ed541bac992c0
ivan <i.telyuk@lerna.tech>
2026-04-07 13:38:59 +0300

    commit

---
+++ src/main/java/Initializer.java
@@ -0,0 +1,64 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.util.Properties;
+
+@Slf4j
+public class Initializer {
+    private static AppiumDriver driver;
+    static Properties config = new Properties();
+
+    static{
+        try (InputStream input = Initializer.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                log.error("Unable to find config.properties");
+            }
+        } catch (Exception ex) {
+            log.error("Failed to load configuration", ex);
+            throw new ExceptionInInitializerError();
+        }
+    }
+
+    public static AppiumDriver getDriver() {
+        if (driver == null) {
+            initDriver();
+        }
+        return driver;
+    }
+
+    private static void initDriver() {
+        try {
+            URI appiumServerURI = new URI(config.getProperty("appium.server.url"));
+            URL appiumServerUrl = appiumServerURI.toURL();
+            driver = new AndroidDriver(appiumServerUrl, getDesiredCapabilities());
+            log.info("Driver initialized successfully");
+        } catch (Exception ex) {
+            log.error("Driver initialized failed");
+            throw new RuntimeException();
+        }
+    }
+
+    private static DesiredCapabilities getDesiredCapabilities() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("deviceName", config.getProperty("device.name"));
+        capabilities.setCapability("platformName", config.getProperty("platform.name"));
+        capabilities.setCapability("app", config.getProperty("app"));
+        capabilities.setCapability("appPackage", config.getProperty("app.package"));
+        capabilities.setCapability("automationName", config.getProperty("automation.name"));
+        capabilities.setCapability("newCommandTimeout", config.getProperty("new.command.timeout"));
+        capabilities.setCapability("appActivity", config.getProperty("app.activity"));
+        return capabilities;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
